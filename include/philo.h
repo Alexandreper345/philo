8d7954ff55cdb3a6d3c2c8d7f96041de859612e0
Alexandre da silva peroba <asilvaperoba@gmail.com>
2025-06-24 21:03:18 -0300

    feat: 🎸 init philo

--- include/philo.h
+++ include/philo.h
@@ -1,4 +1,40 @@
 #ifndef PHILO_H
 # define PHILO_H
+# include <stdbool.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <string.h>
+# include <limits.h>
+# include <errno.h>
+# include <sys/stat.h>
+#include "../Libft/libft.h"
+
+typedef pthread_mutex_t t_mtx;
+typedef long long		t_time;
+
+typedef struct s_philo
+{
+
+} t_philo;
+
+
+typedef struct s_table
+{
+	bool	philo_die;
+	int		*forks;
+	int		size_philos;
+	int		size_philos_eat;	
+	t_philo	**philos;
+	t_mtx	mutex_print;
+	t_time	time_die;
+	t_time	time_eat;
+	t_time	time_sleep;
+	t_time	init_table;
+} t_table;
+
+int	init_table(t_table **table, int argc, char **argv);
 
 #endif
--- main.c
+++ main.c
@@ -6,15 +6,72 @@
 /*   By: alda-sil <alda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 19:01:30 by alda-sil          #+#    #+#             */
-/*   Updated: 2025/06/18 20:41:03 by alda-sil         ###   ########.fr       */
+/*   Updated: 2025/06/24 21:00:56 by alda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/philo.h"
 
+int	isnumeric(char *value)
+{
+	int	i;
+	
+	i = -1;
+	while (value[++i])
+	{
+		printf("entrou %c\n", value[i]);
+		if (ft_isdigit(value[i]))
+			return(0);
+	}
+	printf("oi\n");
+	return (1);
+}
+static int	validate_args(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc < 5 || argc > 6)
+	{
+		printf("[ERROR]: arguments number invalid\n");
+		return (EXIT_FAILURE);
+	}
+	else if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 200)
+	{
+		printf("[ERROR]: first argument invalid\n");
+		return (EXIT_FAILURE);
+	}	
+	while (argv[++i])
+	{
+		printf("pre\n");
+		if (isnumeric(argv[i]))
+		{
+			printf("pos\n");
+			printf("%s\n", argv[i]);
+			printf("[ERROR]: argument invalid\n");
+			return (EXIT_FAILURE);
+		}	
+		else if ((i >= 2 && i <= 4) && ft_atoi(argv[i]) < 60)
+		{
+			printf("[ERROR]: timestap invalid\n");
+			return (EXIT_FAILURE);
+		}	
+		else if (i == 6 && ft_atoi(argv[i]) < 1)
+		{
+			printf("[ERROR]: Invalid sixth argument\n");
+			return (EXIT_FAILURE);
+		}	
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc <= 4 && argc >= 5)
-		return (1);
+	//t_table	*table;
 	
+	if (validate_args(argc, argv))
+		return (EXIT_FAILURE);
+	//else if (init_table(table, argc, argv))
+	//	return (EXIT_FAILURE);
+	return (0);
 }
