a3e9f83062069deeec3b06bf59b33dc9f0c15277
Alexandre da silva peroba <asilvaperoba@gmail.com>
2025-06-25 21:44:42 -0300

    test: 💍 testing args done
    testing args done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alda-sil <alda-sil@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 19:01:30 by alda-sil          #+#    #+#             */
-/*   Updated: 2025/06/24 21:00:56 by alda-sil         ###   ########.fr       */
+/*   Updated: 2025/06/25 21:43:53 by alda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,48 +19,29 @@ int	isnumeric(char *value)
 	i = -1;
 	while (value[++i])
 	{
-		printf("entrou %c\n", value[i]);
-		if (ft_isdigit(value[i]))
-			return(0);
+		if (!ft_isdigit(value[i]))
+			return(1);	
 	}
-	printf("oi\n");
-	return (1);
+	return (0);
 }
+ 
 static int	validate_args(int argc, char **argv)
 {
 	int	i;
 
 	i = 0;
 	if (argc < 5 || argc > 6)
-	{
-		printf("[ERROR]: arguments number invalid\n");
 		return (EXIT_FAILURE);
-	}
 	else if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 200)
-	{
-		printf("[ERROR]: first argument invalid\n");
 		return (EXIT_FAILURE);
-	}	
 	while (argv[++i])
 	{
-		printf("pre\n");
 		if (isnumeric(argv[i]))
-		{
-			printf("pos\n");
-			printf("%s\n", argv[i]);
-			printf("[ERROR]: argument invalid\n");
 			return (EXIT_FAILURE);
-		}	
 		else if ((i >= 2 && i <= 4) && ft_atoi(argv[i]) < 60)
-		{
-			printf("[ERROR]: timestap invalid\n");
 			return (EXIT_FAILURE);
-		}	
-		else if (i == 6 && ft_atoi(argv[i]) < 1)
-		{
-			printf("[ERROR]: Invalid sixth argument\n");
-			return (EXIT_FAILURE);
-		}	
+		else if (i == 5 && ft_atoi(argv[i]) < 1)
+			return (EXIT_FAILURE);	
 	}
 	return (EXIT_SUCCESS);
 }
